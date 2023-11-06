fbfb91568d38663cb16d6fc2bbf15493fcc405a8
seb dubol <seb_dubol@yahoo.fr>
2023-11-06 19:32:30 +0100

    V2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
          <p>
-        Salut je suis papitchou !! Le mec cool !
+        Salut je suis papitchou !! Le mec VRAIMENT cool !
         </p>
 
         <img src={logo} className="App-logo" alt="logo" />
